773031dd00a1485d260bcd1ac9f7a1d1365046d1
--global <hello@simons-email.co.uk>
2016-01-19 10:58:23 -0800

    Fixed last commit, mucked up the merge and deleted written tests

--- tests/unittests/test_pushbullet.py
+++ tests/unittests/test_pushbullet.py
@@ -3,7 +3,7 @@ from pushbullet import pushbullet
 from mock_response import mocked_requests_get, mocked_requests_post, mocked_requests_delete
 
 class TestPushbullet(object):
-    
+
     def setup_method(self, method):
         self.patchers = []
         patcher_get = mock.patch('pushbullet.pushbullet.requests.Session.get', side_effect=mocked_requests_get)
@@ -15,17 +15,34 @@ class TestPushbullet(object):
         self.mocked_requests_delete = patcher_delete.start()
         self.pb = pushbullet.Pushbullet("API_KEY")
         self.device = self.pb.devices[0]
-    
+
     def teardown_method(self, method):
         for patcher in self.patchers:
             patcher.stop()
-        
+
     def test_edit_device_without_nickname(self):
         new_device = self.pb.edit_device(self.device)
         assert new_device.nickname == self.device.nickname
-        
+
     def test_edit_device_with_nickname(self):
         nickname = "New Test Nickname"
         new_device = self.pb.edit_device(self.device, nickname=nickname)
         assert new_device.nickname == nickname
-        
+
+    def test_edit_device_without_model(self):
+        new_device = self.pb.edit_device(self.device)
+        assert new_device.model == self.device.model
+
+    def test_edit_device_with_model(self):
+        model = "New Test Model"
+        new_device = self.pb.edit_device(self.device, model=model)
+        assert new_device.model == model
+
+    def test_edit_device_without_manufacturer(self):
+        new_device = self.pb.edit_device(self.device)
+        assert new_device.model == self.device.model
+
+    def test_edit_device_with_manufacturer(self):
+        manufacturer = "New Test manufacturer"
+        new_device = self.pb.edit_device(self.device, manufacturer=manufacturer)
+        assert new_device.manufacturer == manufacturer
